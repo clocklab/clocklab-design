125cab2a35f9ea8d5ea71b5a3952b3b77bffd15f
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-07-28 18:58:32 +0300

    add meni background

--- source/scripts/components/menu.js
+++ source/scripts/components/menu.js
@@ -1,10 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const openClose = document.querySelector('.menu__item--open-close'),
+    const openCloseBtn = document.querySelector('.menu__item--open-close'),
           menu = document.querySelector('.menu'),
-          menuItems = document.querySelectorAll('.menu__item')
+          menuItems = document.querySelectorAll('.menu__item'),
+          menuBackground = document.querySelector('.menu__background'),
+          animationTime = 1600
 
-    openClose.addEventListener('click', event => {
+    const closeMenu = event => {
         event.preventDefault()
-        menu.classList.toggle('opened')
-    })
+        menu.classList.remove('opened')
+        menuBackground.removeEventListener('click', closeMenu)
+        openCloseBtn.removeEventListener('click', closeMenu)
+
+        setTimeout(() => {
+            openCloseBtn.addEventListener('click', openMenu)
+        }, animationTime)
+    }
+
+    const openMenu = event => {
+        event.preventDefault()
+        menu.classList.add('opened')
+        openCloseBtn.removeEventListener('click', openMenu)
+
+        setTimeout(() => {
+            openCloseBtn.addEventListener('click', closeMenu)
+            menuBackground.addEventListener('click', closeMenu)
+        }, animationTime)
+    }
+
+    openCloseBtn.addEventListener('click', openMenu)
 })
