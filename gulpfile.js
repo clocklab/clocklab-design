952bcdc398f12177146de8c01c7fbeeb1e6e1524
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-07-31 18:59:23 +0300

    add tablet and mobile versions

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ var source = 'source',
             html: source + '/**/*.html',
             fonts: source + '/fonts/*.*',
             videos: source + '/videos/*.*',
-            images: source + '/images/*.*',
+            //images: source + '/images/*.*',
             styles: source + '/styles/**/*.*',
             scripts: source + '/scripts/**/*.*',
             projects: source + '/projects/**/*'
@@ -142,9 +142,9 @@ gulp.task('watch', function() {
     plugins.watch([path.watch.fonts], function(event, cb) {
         gulp.start('fonts');
     });
-    plugins.watch([path.watch.images], function(event, cb) {
-        gulp.start('images');
-    });
+    // plugins.watch([path.watch.images], function(event, cb) {
+    //     gulp.start('images');
+    // });
     plugins.watch([path.watch.videos], function(event, cb) {
         gulp.start('videos');
     });
