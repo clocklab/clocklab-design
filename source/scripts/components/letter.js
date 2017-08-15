49a95dd7405c8198528c619bbf083d525cea8bd2
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-08-15 12:24:25 +0300

    move letter to separate component

---
+++ source/scripts/components/letter.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const selectors = ['.euro-club']
+
+    const move = selectors => {
+
+        selectors.forEach(selector => {
+
+            const parent = document.querySelector(selector),
+                  letter = document.querySelector(`${selector} .letter`),
+                  koef = 10
+
+            parent.addEventListener('mousemove', event => {
+                const allHeight = document.documentElement.clientHeight,
+                      allWidth = document.documentElement.clientWidth,
+                      currentXPos = event.clientX,
+                      currentYPos = event.clientY
+
+                const newX = - (allWidth / 2 - currentXPos) / koef,
+                      newY = - (allHeight / 2 - currentYPos) / koef
+
+                letter.style.transform = `translate3d(${newX}px, ${newY}px, 0)`
+            })
+        })
+    }
+
+    move()
+})
