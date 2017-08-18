226bf8bedf284fcfccffe35d9373fedd13e777ba
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-08-18 17:53:17 +0300

    add scripts

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -4,26 +4,33 @@
 // WowJS
 //= ../../bower_components/wow/dist/wow.js
 
+// shadow
+//= components/shadow.js
+
 // scripts
 //= components/scripts.js
 
 // clock
-
-
-// text shadow
+//= components/clock.js
 
 
 // menu
 //= components/menu.js
 
 // alta
-
+//= components/alta.js
 
 // letter
-
+//= components/letter.js
 
 // button-freaky
+//= components/button-freaky.js
 
+// horizontal-scroll
+//= components/horizontal-scroll.js
+
+// portfolio-shadow
+//= components/portfolio-shadow.js
 
 // slides changing
 //= components/slides-changing.js
