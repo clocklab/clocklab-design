86128fe71f97c1e1cf6d2b65fbcfaf9450cafa51
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-08-28 10:29:28 +0300

    added some changes

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -17,17 +17,31 @@
 //= components/menu.js
 
 // alta
+//= components/alta.js
 
 // letter
 //= components/letter.js
 
+// slides-changing
+//= components/slides-changing.js
+
 // button-freaky
+//= components/button-freaky.js
 
 // horizontal-scroll
+//= components/horizontal-scroll.js
 
 // horizontal-scroll-blog
 //= components/horizontal-scroll-blog.js
 
 // portfolio-shadow
+//= components/portfolio-shadow.js
 
 // slides changing
+//= components/slides-changing.js
+
+// transition-start
+//= components/transition-start.js
+
+// transition-end
+//= components/transition-end.js
