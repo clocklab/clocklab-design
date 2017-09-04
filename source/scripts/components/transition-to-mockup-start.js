abb6b4459b2e99b6ab093344b55a466adcf1db12
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-09-04 16:31:41 +0300

    finish transition to mockup start

---
+++ source/scripts/components/transition-to-mockup-start.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const animate = (draw, duration) => {
+        const start = performance.now();
+        
+        requestAnimationFrame(function animate(time) {
+            let timePassed = time - start;
+        
+            timePassed = timePassed > duration
+            ? duration
+            : timePassed
+        
+            draw(timePassed);
+        
+            timePassed < duration &&
+            requestAnimationFrame(animate)
+        });
+    }
+
+    const links = document.querySelectorAll('.portfolio .front-layer .image-container')
+    const animationTime = 900
+
+    links.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault()
+            
+            const href = link.getAttribute('href')
+            const image = link.firstElementChild
+            const topPos = image.getBoundingClientRect().top / document.body.clientHeight * 100
+            const leftPos = image.getBoundingClientRect().left / document.body.clientWidth * 100
+
+            image.style.top = `${topPos}%`
+            image.style.left = `${leftPos}%`
+
+            localStorage.setItem('mockup', JSON.stringify({'mockup-transition': true}))
+
+            link.classList.add('active')
+            link.parentElement.parentElement.style.zIndex = '9999'
+
+            animate((timePassed) => {
+                image.style.top = `${topPos + timePassed / (animationTime / (25 - topPos))}%`
+                image.style.left = `${leftPos + timePassed / (animationTime / (25 - leftPos))}%`
+            }, animationTime)
+
+            setTimeout(() => {
+                document.location.href = href
+            }, animationTime + 100)
+        })
+    })
+})
