9e8ca87ca6f03850f07ae418bae3170c7a3e836b
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-09-12 17:15:58 +0300

    finish site-types and work-flow

--- source/scripts/components/open-form.js
+++ source/scripts/components/open-form.js
@@ -1,6 +1,22 @@
 const openBtn = document.querySelector('.questions .open-form'),
-      closeBtn = document.querySelector('.questions .close-form'),
-      frontLayer = document.querySelector('.front-layer')
+      closeBtn = document.querySelector('.back-layer .close-form'),
+      workFlow = document.querySelector('.work-flow'),
+      menu = document.querySelector('.front-layer .menu'),
+      animationTime = 700;
 
-openBtn.addEventListener('click', () => frontLayer.classList.add('.opened'))
-closeBtn.addEventListener('click', () => frontLayer.classList.remove('.opened'))
+openBtn.addEventListener('click', () => {
+    workFlow.classList.add('opened')
+    menu.style.perspective = '10px'
+    workFlow.style.perspective = '800px'
+})
+
+closeBtn.addEventListener('click', () => {
+    event.preventDefault()
+
+    workFlow.classList.remove('opened')
+
+    setTimeout(() => {
+        menu.removeAttribute('style')
+        workFlow.removeAttribute('style')
+    }, animationTime)
+})
