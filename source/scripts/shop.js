1e010a6d696dd6b031aad43cb6ea4f1fab0c1f98
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-09-15 18:15:56 +0300

    finish web-production second level pages

---
+++ source/scripts/shop.js
@@ -0,0 +1,18 @@
+// menu
+//= components/menu.js
+
+//= components/jquery.min.js
+
+//= components/wow.js
+
+//= components/clock-bg.js
+
+//= components/web-production-clock-bg.js
+
+//= components/big-background-letter.js
+
+//= components/web-production-questions.js
+
+//= components/web-production-open-form.js
+
+new WOW().init();
