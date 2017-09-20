0e6fd7837a3d42cb7c790242b48eec52bc600343
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-09-20 17:47:50 +0300

    finish complex page

---
+++ source/scripts/complex.js
@@ -0,0 +1,16 @@
+// menu
+//= components/menu.js
+
+//= components/jquery.min.js
+
+//= components/wow.js
+
+//= components/big-background-letter.js
+
+//= components/web-production-questions.js
+
+//= components/web-production-open-form.js
+
+//= components/complex-time.js
+
+new WOW().init();
