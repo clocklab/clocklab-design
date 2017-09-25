eea128a2437056ded411ec9ae53cdc2fe19b0be4
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-09-25 16:05:50 +0300

    finish chain for index page

--- source/scripts/components/slides-changing.js
+++ source/scripts/components/slides-changing.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     const slides = Array.prototype.slice.call( document.querySelectorAll('.screen-block'))
-    const events = [/*'wheel', 'scroll', */'keydown']
+    const events = ['wheel', 'keydown']
     const animationTime = 1000
-    let freezer
     let flag = true
+    let firstLap = true
+    let freezer
     
     const addListeners = () => events.forEach(event => document.addEventListener(event, freezeEvents))
 
@@ -31,36 +32,44 @@ document.addEventListener('DOMContentLoaded', () => {
         ? slides[index - 1].classList.add('current-back')
         : slides[slides.length - 1].classList.add('current-back')
     }
+
+    const setClasses = (currentSlide, previousSlide, currentClass) => {
+        previousSlide && previousSlide.classList.remove('previous', 'previous-back');
+        currentSlide.classList.remove('current', 'current-back');
+        currentSlide.classList.add(currentClass);
+    }
+
+    const resetAnimation = (currentSlide, currentClass) => {
+        setTimeout(() => {
+            currentSlide.classList.remove(currentClass);
+        }, animationTime)
+    }
     
     const move = () => {
         const previousSlide = document.querySelector('.screen-block.previous') || document.querySelector('.screen-block.previous-back');
         const currentSlide = document.querySelector('.screen-block.current') || document.querySelector('.screen-block.current-back');
         const index = slides.indexOf(currentSlide);
         
-        if (/*event.deltaY < 0 || */event.keyCode === 40) {
-            previousSlide && previousSlide.classList.remove('previous', 'previous-back');
-            currentSlide.classList.remove('current', 'current-back');
-            currentSlide.classList.add('previous');
-            
-            //reset animation
-            setTimeout(() => {
-                currentSlide.classList.remove('previous');
-            }, animationTime)
-
+        if (event.deltaY < 0 || event.keyCode === 40) {
+            setClasses(currentSlide, previousSlide, 'previous')
+            resetAnimation(currentSlide, 'previous')
             moveToNext(index)
+
+            if (index === slides.length - 1) firstLap = false
         }
 
-        if (/*event.deltaY > 0 || */event.keyCode === 38) {
-            previousSlide && previousSlide.classList.remove('previous', 'previous-back');
-            currentSlide.classList.remove('current', 'current-back');
-            currentSlide.classList.add('previous-back');  
-
-            //reset animation
-            setTimeout(() => {
-                currentSlide.classList.remove('previous-back');
-            }, animationTime)
-
-            moveToPrevious(index)
+        if (event.deltaY > 0 || event.keyCode === 38) {
+            if (firstLap) {
+                if (slides[index - 1]) {
+                    setClasses(currentSlide, previousSlide, 'previous-back')
+                    resetAnimation(currentSlide, 'previous-back')
+                    slides[index - 1].classList.add('current-back')
+                }
+            } else {
+                setClasses(currentSlide, previousSlide, 'previous-back')
+                resetAnimation(currentSlide, 'previous-back')
+                moveToPrevious(index)
+            }
         }
     }
     
