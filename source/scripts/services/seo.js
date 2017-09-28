e0e7aa6e3530ee68c9fdd6c9a865f32fb213909a
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-09-28 10:07:18 +0300

    add contacts page

---
+++ source/scripts/services/seo.js
@@ -0,0 +1,15 @@
+// jQuery
+//=include ./../libraries/jquery.min.js
+
+// slides-changing
+//=include ./../components/slides-changing.js
+
+// big-background-letter
+//=include ./../components/big-background-letter.js
+
+// menu
+//=include ./../components/menu.js
+
+// ----- change lines and language color -----
+$('.light div').children().addClass('light');
+$('.dark').children().addClass('dark');
