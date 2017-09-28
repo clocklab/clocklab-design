24b3ba56e52b0f4315c2da8d7c08690f2f2d2967
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-09-28 18:55:46 +0300

    finished main-contacts

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,9 +1,12 @@
-//=include ./libraries/jquery.min.js
+//= include ./libraries/jquery.min.js
 
-//=include ./components/slides-changing.js
+//= include ./components/slides-changing.js
 
-//=include ./components/big-background-letter.js
+//= include ./components/big-background-letter.js
 
-//=include ./modules/index/alta.js
+//= include ./modules/index/alta.js
 
-//=include ./components/clock.js
+//= include ./components/clock.js
+
+// ----- change lines color -----
+$('.light div').children().addClass('light');
