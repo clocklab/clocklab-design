65c54511be133a7536af98b6c1283b97bf6310e1
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-09-29 18:00:23 +0300

    added about-company text

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,6 +2,8 @@
 
 //= include ./components/slides-changing.js
 
+//= include ./services/modules/open-long-read.js
+
 //= include ./components/big-background-letter.js
 
 //= include ./components/jelly-button.min.js
@@ -11,6 +13,8 @@
 //= include ./modules/index/alta.js
 
 //= include ./components/clock.js
+
+//= include ./libraries/wow.js
 
 // ----- change lines color -----
 $('.light div').children().addClass('light');
