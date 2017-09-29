6197403368b4375c20d7b18ecd96b632a70a7f40
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-09-29 19:34:15 +0300

    added seo text

--- source/scripts/services/design.js
+++ source/scripts/services/design.js
@@ -1,18 +1,17 @@
-//= include ./../libraries/jquery.min.js
-
-//= include ./../libraries/wow.js
-
-//= include ./../components/menu.js
-
 //= include modules/open-long-read.js
 
-//= include ./../components/open-form.js
-
-//= include ./../components/slides-changing.js
+//= include ./../libraries/jquery.min.js
 
 //= include ./../components/big-background-letter.js
 
-// ----- change lines and language color -----
+//= include ./../components/slides-changing.js
 
+//= include ./../components/open-form.js
+
+//= include ./../components/menu.js
+
+//= include ./../libraries/wow.js
+
+// ----- change lines and language color -----
 $('.light div').children().addClass('light');
 $('.dark').children().addClass('dark');
--- source/scripts/services/seo.js
+++ source/scripts/services/seo.js
@@ -1,10 +1,16 @@
-//= include ./../libraries/jquery.min.js
+//= include modules/open-long-read.js
 
-//= include ./../components/slides-changing.js
+//= include ./../libraries/jquery.min.js
 
 //= include ./../components/big-background-letter.js
 
+//= include ./../components/slides-changing.js
+
+//= include ./../components/open-form.js
+
 //= include ./../components/menu.js
+
+//= include ./../libraries/wow.js
 
 // ----- change lines and language color -----
 $('.light div').children().addClass('light');
