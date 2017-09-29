c46b30c9ef5b8c26dcd408d46720cf33cd830dda
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-09-29 09:53:48 +0300

    add marketing long read

--- source/scripts/services/marketing.js
+++ source/scripts/services/marketing.js
@@ -1,5 +1,11 @@
 //=include ./../components/menu.js
 
+//=include ./../libraries/jquery.min.js
+
+//=include ./../libraries/wow.js
+
 //=include ./../components/slides-changing-services.js
 
 //=include modules/marketing/marketing-open-form.js
+
+//=include modules/open-long-read.js
