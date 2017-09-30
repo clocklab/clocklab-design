71a454c929cb22d6a4b9325b63f4e2d7c4848f26
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-09-30 18:32:12 +0300

    assembled a project

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
         pattern: '*'
     });
 
-var page = 'services'
+var page = 'services/web-production/shop'
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
