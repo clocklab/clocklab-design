cddc076d6aeed2fb30ffb128da25fde6faa591bc
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-10-02 12:59:50 +0300

    fix long read on the index page

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
         pattern: '*'
     });
 
-var page = 'services/web-production/shop'
+var page = 'index'
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
