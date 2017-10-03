5fa85cacddc6461beadf91d5e5180817c90ea180
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-10-03 13:09:07 +0300

    add scroll menu

---
+++ source/scripts/services/modules/scroll-menu.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const topPanel = document.querySelector('.top-panel')
+    const delay = 300
+    let freezer
+
+    document.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop !== 0) {
+            !topPanel.classList.contains('active') && topPanel.classList.add('active')
+            !topPanel.classList.contains('scrolled') && topPanel.classList.add('scrolled')
+
+            freezer && clearTimeout(freezer)
+            freezer = setTimeout(() => {
+                topPanel.classList.remove('scrolled')
+            }, delay)
+        } else {
+            topPanel.classList.contains('active') && topPanel.classList.remove('active', 'scrolled')
+        }
+    })
+})
