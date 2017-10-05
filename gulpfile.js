faa33398a2ec71cc0cfccda9c5a78e21870722e7
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-10-05 17:47:14 +0300

    fixed seo pages

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
         pattern: '*'
     });
 
-var page = 'services/design/brand-book'
+var page = 'services/seo/creating'
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
