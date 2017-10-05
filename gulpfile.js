69df591c96312377be75bf1860a4ec9e258e5e17
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-10-05 13:30:28 +0300

    fix web-production pages

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
         pattern: '*'
     });
 
-var page = 'services/complex'
+var page = 'services/web-production/shop'
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
