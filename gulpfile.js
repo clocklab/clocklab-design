baeeae3cbc4c36a4b0d4807cb0ece21a7c4da7ac
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-10-05 16:50:11 +0300

    fix portfolio and hide language

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
         pattern: '*'
     });
 
-var page = 'services/marketing'
+var page = 'portfolio'
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
