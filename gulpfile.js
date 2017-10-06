4d698b659b5cc0f185741bb505650e09d7322769
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-10-06 15:33:31 +0300

    assembled contacts page

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
         pattern: '*'
     });
 
-var page = 'services/seo'
+var page = 'contacts'
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
