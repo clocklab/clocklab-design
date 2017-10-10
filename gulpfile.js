8c03120e9b7fb823b6b03705f35f7cd470b37319
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-10-10 11:42:03 +0300

    fix wheel event for mozilla

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
         pattern: '*'
     });
 
-var page = 'blog'
+var page = 'web-production'
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
