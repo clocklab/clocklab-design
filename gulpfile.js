0e7354340584029b9bfa70d0c234e8b7f788b0b7
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-10-12 20:56:02 +0300

    added fonts to the web production pages

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
 var mobile = ''
 // var mobile = '-mobile'
 
-var page = `services/marketing/internet-marketing`
+var page = `services/web-production/shop`
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
