e275f2d0a07a8719f3cc47316e7893284d58a5f7
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-10-13 16:09:52 +0300

    fix flashing

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
 var mobile = '-mobile'
 var mobile = ''
 
-var page = `services/web-production/shop${mobile}`
+var page = `index${mobile}`
 
 
 var sass = require('gulp-ruby-sass');
