056766c7e4f13b9ad3cb6e026629c86a36770c6a
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-10-26 18:58:30 +0300

    remove headers

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var gulp = require('gulp'),
     });
 
 var mobile = '-mobile'
-var mobile = ''
+// var mobile = ''
 
-var page = `portfolio${mobile}`
+var page = `services${mobile}`
 
 
 var sass = require('gulp-ruby-sass');
