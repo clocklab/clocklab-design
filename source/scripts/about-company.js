6241430609ba386b77b7fc5d3edc3bd458bd9484
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-10-27 14:56:11 +0300

    remade for landing about company page

--- source/scripts/about-company.js
+++ source/scripts/about-company.js
@@ -6,14 +6,19 @@
 
 //= include ./components/jelly-button.min.js
 
-//= include ./services/modules/open-long-read.js
-
 //= include ./components/big-background-letter.js
 
-//= include ./components/slides-changing-services.js
-
 //= include ./services/modules/web-production/web-production-open-form.js
+
+//= include ./services/modules/scroll-menu.js
+
+/*
+//= include ./services/modules/open-long-read.js
+//= include ./components/slides-changing-services.js
+*/
 
 // ----- change lines and language color -----
 $('.light div').children().addClass('light');
 $('.dark').children().addClass('dark');
+
+new WOW().init();
