6e91bdec9eac634e16958da1395f59cda74b82d2
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-11-07 10:16:48 +0200

    separate logo and menu components and add scrolled styles for topPanel

--- source/scripts/services/modules/scroll-menu.js
+++ source/scripts/services/modules/scroll-menu.js
@@ -1,13 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const topPanel = document.querySelector('.top-panel')
+    const blocks = document.querySelectorAll('.global-container > div');
     const delay = 500
-    let freezer
 
     document.addEventListener('scroll', () => {
         document.documentElement.scrollTop !== 0 || document.body.scrollTop !== 0
         ? !topPanel.classList.contains('scrolled') && topPanel.classList.add('scrolled')
         : topPanel.classList.contains('scrolled') && topPanel.classList.remove('scrolled')
+
+        blocks.forEach(block => {
+            if (block.getBoundingClientRect().top <= 0 && block.getBoundingClientRect().bottom <= 0) {
+                const background = getComputedStyle(slides[index]).backgroundColor.replace(/rgba?|\(|\)/g, '').split(', ')
+
+                background.filter(number => number < 100).length
+                ? topPanel.classList.remove('dark')
+                : topPanel.classList.add('dark')
+            } 
+        })
     })
+
+
 
     // setTopPanelStyle = () => {
     //     const firstBlock = 
