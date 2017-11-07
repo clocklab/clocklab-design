d0506e4f4dd9e7524c7dc3223f1b29ad4d7ab03f
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-11-07 12:21:07 +0200

    added networks to the contacts page

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,21 +2,20 @@
 
 //= include ./services/modules/web-production/web-production-open-form.js
 
+//= include ./components/big-background-letter.js
+
 //= include ./libraries/jquery.min.js
 
 //= include ./components/clock.js
 
 //= include ./libraries/wow.js
 
-//= include ./modules/index/alta.js
-
 //= include ./components/clock-bg.js
 
-//= include ./components/jelly-button.min.js
+// = include ./components/menu.js
 
-//= include ./components/big-background-letter.js
-
-//= include ./components/menu.js
+// //= include ./modules/index/alta.js
+// //= include ./components/jelly-button.min.js
 
 // ----- change lines color -----
 $('.light div').children().addClass('light');
