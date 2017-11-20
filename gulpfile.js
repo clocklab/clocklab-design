ceffffda693ccde1d981faac8f877ecd3868a928
StadniukYevhen <stadniuk.yevhen@gmail.com>
2017-11-20 12:47:23 +0200

    add new click area on blog page

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var gulp = require('gulp'),
     });
 
 var mobile = '-mobile'
-// var mobile = ''
+var mobile = ''
 
-var page = `index${mobile}`
+var page = `blog${mobile}`
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
