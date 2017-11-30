c04650b5e1b5fed8eab6c9a348896cc9a5b223b3
Igor Gontarenko <igor.gontarenko@gmail.com>
2017-11-30 16:06:04 +0200

    finished complex page

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
 var mobile = '-mobile'
 var mobile = ''
 
-var page = `services/marketing/internet-marketing${mobile}`
+var page = `services/complex${mobile}`
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
