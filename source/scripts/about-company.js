7a5c347160bbc68ebab2ecc5cb45e601aa7e29a3
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-02-14 18:32:42 +0200

    remove unnessasery script imports

--- source/scripts/about-company.js
+++ source/scripts/about-company.js
@@ -4,21 +4,10 @@
 
 //= include ./components/menu.js
 
-//= include ./components/jelly-button.min.js
-
 //= include ./components/big-background-letter.js
 
 //= include ./services/modules/web-production/web-production-open-form.js
 
 //= include ./services/modules/scroll-menu.js
-
-/*
-//= include ./services/modules/open-long-read.js
-//= include ./components/slides-changing-services.js
-*/
-
-// ----- change lines and language color -----
-$('.light div').children().addClass('light');
-$('.dark').children().addClass('dark');
 
 new WOW().init();
