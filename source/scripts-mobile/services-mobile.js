344328ea8b55190f2401001c9e5e60d3d6333a03
Igor Gontarenko <igor.gontarenko@gmail.com>
2018-03-04 23:36:42 +0200

    added services page

--- source/scripts-mobile/services-mobile.js
+++ source/scripts-mobile/services-mobile.js
@@ -1,1 +1,17 @@
-//=include ./components/menu.js
+//= include ./libraries/jquery-3.2.1.min.js
+
+//= include ./libraries/wow.js
+
+//= include ./components/menu.js
+
+//= include ./components/scroll-menu.js
+
+new WOW().init();
+
+$('.toggle').click(function() {
+	if ($(this).hasClass('active')) {
+		$(this).removeClass('active');
+	} else {
+		$(this).addClass('active');
+	}
+});
