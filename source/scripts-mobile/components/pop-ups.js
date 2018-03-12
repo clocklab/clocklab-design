dd64ce94fab258f426bac68d5d75d0d24e939a9a
Igor Gontarenko <igor.gontarenko@gmail.com>
2018-03-12 17:05:14 +0200

    added portfolio tablet page

--- source/scripts-mobile/components/pop-ups.js
+++ source/scripts-mobile/components/pop-ups.js
@@ -1,31 +1,45 @@
 var opened = false;
-$('.open-work').text('Информация о проекте');
 
+// view information about the project
 $('.open-work').click(function() {
+    var name = $(this).data('name');
+
     (!opened) ? (
-        $('.logo, .menu').fadeOut(190),
-        $('.opened-work').addClass('opened fadeInWork'),
+        $('#' + name + ' .opened-work').addClass('opened fadeInWork'),
+        $('#' + name + ' .opened-work').removeClass('fadeOutWork'),
         $('.work-title, .dots, .skip').addClass('transition'),
-        $('.opened-work').removeClass('fadeOutWork'),
         $('.work, .portfolio').bind('touchmove', false),
         window.setTimeout(function() {
-            $('.open-work').text('Закрыть').addClass('white');
+            $('#' + name + ' .open-work').text('Закрыть').addClass('white');
         }, 190)
     ) : (
-        $('.logo, .menu').fadeIn(190),
-        $('.opened-work').removeClass('opened fadeInWork'),
+        $('#' + name + ' .opened-work').removeClass('opened fadeInWork'),
+        $('#' + name + ' .opened-work').addClass('fadeOutWork'),
         $('.work-title, .dots, .skip').removeClass('transition'),
-        $('.opened-work').addClass('fadeOutWork'),
         $('.work, .portfolio').unbind('touchmove', false),
         window.setTimeout(function() {
-            $('.open-work').text('Информация о проекте').removeClass('white');
+            $('#' + name + ' .open-work').text('Информация о проекте').removeClass('white');
         }, 190)
     );
+
+    // disable the animation of logo and menu hiding on the iPad
+    (screen.width < 768 && !opened) ?
+        $('.logo, .menu').fadeOut(190) :
+        $('.logo, .menu').fadeIn(190);
 
     opened = !opened;
 });
 
-var link = [
+// animation of logo and menu hiding when the form appears
+$('.open-form, .close-form').click(function() {
+    (!opened) ?
+        $('.logo, .menu').fadeOut(190) :
+        $('.logo, .menu').fadeIn(190);
+        
+    opened = !opened;
+});
+
+link = [
   "/razrabotka-i-prodvizhenie-sayta-audi",
   "/razrabotka-langing-page-euroclub",
   "/razrabotka-sayta-dokument-servisa-dela-ua"
@@ -37,12 +51,4 @@ $(function() {
     addLink.click(function() {
         $('.open-link a').attr('href', link[length]);
     });
-});
-
-$('.open-form, .close-form').click(function() {
-    (!opened) ?
-        $('.logo, .menu').fadeOut(190) :
-        $('.logo, .menu').fadeIn(190);
-        
-    opened = !opened;
 });
