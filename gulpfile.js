458e904291528c11b7d5261d9db3c1fb7419ef55
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-04-26 13:03:08 +0300

    add scripts for contacts page

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
 var mobile = '-mobile'
 var mobile = ''
 
-var page = `about-company${mobile}`
+var page = `contacts${mobile}`
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
