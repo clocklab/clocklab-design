41b1cb1090c96994431e40fe6669bcd838b6df61
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-04-26 11:49:20 +0300

    create scripts for web-production-second-level template

---
+++ source/scripts/services/web-production/web-production-second-level.js
@@ -0,0 +1,12 @@
+//=include ./../../components/questions.js
+
+//=include ./../../components/open-form.js
+
+//=include ./../../components/scroll-menu.js
+
+new WOW().init();
+
+$('#clock-bg').textshadow({
+    color: 'rgba(0, 0, 0, 1)',
+    textShadow: true
+});
