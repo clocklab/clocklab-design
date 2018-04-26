60cd6e844d59187014b8682b1799ddf4402b5d0d
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-04-26 13:42:01 +0300

    add script on index page

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,21 +1,6 @@
 //= include ./components/slides-changing.js
 
-//= include ./services/modules/web-production/web-production-open-form.js
-
-//= include ./components/big-background-letter.js
-
-//= include ./libraries/jquery.min.js
-
 //= include ./components/clock.js
-
-//= include ./libraries/wow.js
-
-//= include ./components/clock-bg.js
-
-// = include ./components/menu.js
-
-// //= include ./modules/index/alta.js
-// //= include ./components/jelly-button.min.js
 
 // ----- change lines color -----
 $('.light div').children().addClass('light');
