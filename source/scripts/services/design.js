eb9293c47cd27a1f879999820c43357a15268c33
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-05-02 15:03:08 +0300

    finish styles for design page template

--- source/scripts/services/design.js
+++ source/scripts/services/design.js
@@ -1,9 +1,3 @@
 //=include ./../components/open-form.js
 
 //=include ./../components/scroll-menu.js
-
-// ----- change lines and language color -----
-$('.light div').children().addClass('light');
-$('.dark').children().addClass('dark');
-
-new WOW().init();
