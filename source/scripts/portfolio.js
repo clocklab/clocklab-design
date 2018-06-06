a7b50ce095e024bef52ce18b93f284ef5399ad61
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-06-06 18:41:27 +0300

    add active state animation and structure of new portfolio item

--- source/scripts/portfolio.js
+++ source/scripts/portfolio.js
@@ -4,3 +4,12 @@ $('#clock-bg').textshadow({
     color: 'rgba(0, 0, 0, .2)',
     textShadow: true
 });
+
+const buttons = document.querySelectorAll('.info .animated-button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        button.classList.toggle('active');
+        button.parentElement.parentElement.parentElement.classList.toggle('active');
+    });
+});
