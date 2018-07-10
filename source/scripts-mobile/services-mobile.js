73a40fefc34347f4bd733e47648a7daec896c5b9
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-07-10 14:57:44 +0300

    add tablet styles

--- source/scripts-mobile/services-mobile.js
+++ source/scripts-mobile/services-mobile.js
@@ -1,17 +1,61 @@
-//= include ./libraries/jquery-3.2.1.min.js
-
-//= include ./libraries/wow.js
-
-//= include ./components/menu.js
-
-//= include ./components/scroll-menu.js
-
-new WOW().init();
-
-$('.toggle').click(function() {
-	if ($(this).hasClass('active')) {
-		$(this).removeClass('active');
-	} else {
-		$(this).addClass('active');
+;
+(function() {
+	const animate = (draw, duration) => {
+		const start = performance.now();
+	  
+		requestAnimationFrame(function animate(time) {
+			let timePassed = time - start;
+		
+			timePassed = timePassed > duration
+			? duration
+			: timePassed
+		
+			draw(timePassed);
+		
+			timePassed < duration &&
+			requestAnimationFrame(animate)
+		});
 	}
-});
+
+	var itemTogglers = [].slice.call(document.querySelectorAll('.item-toggler'));
+	var lists = [].slice.call(document.querySelectorAll('.inner-list'));
+	var animationTime = 300;
+	var toggleClass = 'active';
+
+
+	function clickHandler()	{
+		this.removeEventListener('click', clickHandler);
+		var submenu = lists[itemTogglers.indexOf(this)];
+
+		if (this.classList.contains(toggleClass)) {
+			var wantedHeight = submenu.offsetHeight;
+
+			animate((timePassed) => {
+				submenu.style.height = `${wantedHeight - timePassed / (animationTime / wantedHeight)}px`;
+			}, animationTime);
+
+			setTimeout(() => {
+				submenu.style.height = '';
+				submenu.style.display = '';
+				this.addEventListener('click', clickHandler);
+			}, animationTime + 100);
+		} else {
+			submenu.style.display = 'block';
+			var wantedHeight = submenu.offsetHeight;
+			submenu.style.height = '0px';
+		
+			animate((timePassed) => {
+				submenu.style.height = `${timePassed / (animationTime / wantedHeight)}px`
+			}, animationTime);
+		
+			setTimeout(() => {
+				submenu.style.height = '';
+				this.addEventListener('click', clickHandler);
+			}, animationTime + 100);
+		}
+
+		this.classList.toggle(toggleClass);
+	}
+
+	itemTogglers.forEach((itemToggler) => itemToggler.addEventListener('click', clickHandler));
+})();
