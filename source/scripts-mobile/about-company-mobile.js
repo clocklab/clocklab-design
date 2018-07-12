85b8ea28a2c840c7518e1bd82c4d548aacd15e6c
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-07-12 13:55:26 +0300

    finish about-company page mobile version and change content part

--- source/scripts-mobile/about-company-mobile.js
+++ source/scripts-mobile/about-company-mobile.js
@@ -1,15 +1,75 @@
-//= include ./libraries/jquery-3.2.1.min.js
+function sliderForClients() {
+    const sliderClients = document.querySelector('.slider-clients');
+    const slides = sliderClients.querySelectorAll('.slide');
+    const dots = document.querySelector('.our-clients .dots');
+    const minDif = 0.2;
+    const limit = - (slides.length - 1) * 100;
+    const animationTime = 300;
+    let koef = 0.8;
+    let sliderClientsWrapperWidth = null;
+    let startTouchPos = null;
+    let startLeftValue = null;
+    let lastMove = null;
 
-//= include ./libraries/wow.js
+    const moveSlider = event => {
+        lastMove = event
 
-//= include ./components/menu.js
+        const endTouchPos = lastMove.touches[0].clientX
+        const previousLeftValue = parseInt(sliderClients.style.left) || 0 
 
-//= include ./components/open-form.js
+        sliderClients.style.left = `${startLeftValue + (endTouchPos - startTouchPos) / sliderClientsWrapperWidth * 100 * koef}%`
+    }
 
-//= include ./components/scroll-menu.js
+    const changeActiveDot = direction => {
+        const activeDot = dots.querySelector('.active')
 
-//= include ./components/slider-clients.js
+        activeDot.classList.remove('active')
+        direction === 'next'
+        ? activeDot.nextElementSibling.classList.add('active')
+        : activeDot.previousElementSibling.classList.add('active')
+    }
 
-//= include ./components/pop-ups.js
+    const setLeftPosition = () => {
+        const endTouchPos = lastMove && lastMove.touches[0].clientX
+        const dif = Math.abs(endTouchPos - startTouchPos) / sliderClientsWrapperWidth
 
-new WOW().init();
+        sliderClients.style.transition = `left ${animationTime}ms ease`
+
+        sliderClients.style.left = dif < minDif
+        ? `${startLeftValue}%`
+        :
+            endTouchPos > startTouchPos
+            ? 
+                startLeftValue !== 0
+                ? `${startLeftValue + 100}%`
+                : `${startLeftValue}%`
+            : 
+                startLeftValue !== limit  
+                ? `${startLeftValue - 100}%`
+                : `${startLeftValue}%`
+
+        if (dif > minDif) {
+            endTouchPos > startTouchPos
+            ? startLeftValue !== 0 && changeActiveDot('prev')
+            : startLeftValue !== limit && changeActiveDot('next')
+        }
+
+        setTimeout(() => {
+            sliderClients.style.transition = null
+        }, animationTime + 100)
+        
+        sliderClients.removeEventListener('touchmove', moveSlider)
+        sliderClients.removeEventListener('touchend', setLeftPosition)
+    }
+
+    sliderClients.addEventListener('touchstart', event => {
+        sliderClientsWrapperWidth = sliderClients.parentElement.getBoundingClientRect().width
+        startTouchPos = event.touches[0].clientX
+        startLeftValue = parseInt(sliderClients.style.left) || 0
+
+        sliderClients.addEventListener('touchmove', moveSlider)
+        sliderClients.addEventListener('touchend', setLeftPosition)
+    })
+}
+
+sliderForClients()
