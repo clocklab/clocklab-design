3a28dbc48e49e894d077b5747c65e691c77f37d6
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-07-17 13:58:06 +0300

    create portfolio mobile template

--- source/scripts-mobile/portfolio-mobile.js
+++ source/scripts-mobile/portfolio-mobile.js
@@ -1,13 +1,42 @@
-//= include ./libraries/jquery-3.2.1.min.js
+//    //= include ./libraries/jquery-3.2.1.min.js
 
-//= include ./libraries/wow.js
+//    //= include ./libraries/wow.js
 
-//= include ./components/menu.js
+//    //= include ./components/menu.js
 
-//= include ./components/scroll-menu.js
+//    //= include ./components/scroll-menu.js
 
-//= include ./components/pop-ups.js
+//    //= include ./components/pop-ups.js
 
-//= include ./components/anchors.js
+//    //= include ./components/anchors.js
 
-new WOW().init();
+//    new WOW().init();
+
+
+;(function() {
+    var btns = document.querySelectorAll('.case__open-btn');
+
+    function openClose(el) {
+        const openedElement = document.querySelector('.opened');
+        (openedElement && openedElement !== el) && openedElement.classList.remove('opened');
+
+        if (!el.matches('.opened')) {
+            const windowScroll = window.pageYOffset;
+            const elScroll = el.getBoundingClientRect().top;
+            const wantedScroll = windowScroll + elScroll;
+
+            window.scroll({
+                top: wantedScroll,
+                behavior: 'smooth'
+            });
+        }
+         
+        el.classList.toggle('opened');
+    }
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            openClose(btn.parentElement)
+        });
+    })
+})();
