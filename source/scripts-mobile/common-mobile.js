0f5cd96339166cdbf8c54f97cdd1e2ab787a3d64
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-07-18 17:09:52 +0300

    add light version of logo and menu

--- source/scripts-mobile/common-mobile.js
+++ source/scripts-mobile/common-mobile.js
@@ -31,14 +31,44 @@
 ;(function() {
     const logo = document.querySelector('#logo');
     const menu = document.querySelector('#menu');
+    const blocks = document.querySelectorAll('[data-items-color]');
+    const DELTA = 20;
 
-    window.addEventListener('scroll', () => {
-        if (window.pageYOffset) {
-            logo.classList.add('scrolled')
-            menu.classList.add('scrolled')
-        } else {
-            logo.classList.remove('scrolled');
-            menu.classList.remove('scrolled');
-        }
-    });
+    const addClasses = (arr, wantedClass) => {
+        arr.forEach(el => {
+            !el.matches(`.${wantedClass}`) && el.classList.add(wantedClass);
+        });
+    }
+
+    const removeClasses = (arr, unwantedClass) => {
+        arr.forEach(el => {
+            el.matches(`.${unwantedClass}`) && el.classList.remove(unwantedClass);
+        });
+    }
+
+    const checkCurrentBlockBackground = () => {
+        blocks.forEach(block => {
+            if (block.getBoundingClientRect().top <= DELTA && block.getBoundingClientRect().bottom >= DELTA) {
+                block.dataset.itemsColor === 'light'
+                ? addClasses([logo, menu], 'light')
+                : removeClasses([logo, menu], 'light');
+            }
+        })
+    }
+
+    const checkWindowTopPos = () => {
+        window.pageYOffset
+        ? addClasses([logo, menu], 'scrolled')
+        : removeClasses([logo, menu], 'scrolled');
+    }
+
+    if (blocks.length > 1) {
+        window.addEventListener('scroll', checkCurrentBlockBackground);
+        window.addEventListener('resize', checkCurrentBlockBackground);
+    }
+
+    window.addEventListener('scroll', checkWindowTopPos);
+
+    checkCurrentBlockBackground();
+
 })();
