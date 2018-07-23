3921baa2920a0a1f24049828b99d792bb77e9d2f
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-07-23 11:50:46 +0300

    remove unnecassery scripts

--- source/scripts-mobile/index-mobile.js
+++ source/scripts-mobile/index-mobile.js
@@ -1,19 +1,104 @@
-//    //= include ./libraries/jquery-3.2.1.min.js
+var cat = [
+    "Web-production",
+    "Услуги дизайна",
+    "Маркетинг",
+    "SEO",
+    "Комплексные решения"
+],
+subСat = [
+    "Landing page <br> Интернет магазин <br> Корпоративный сайт <br> Сайт визитка",
+    "UI / UX дизайн <br> Разработка Brand Book",
+    "Разработка маркетинговой стратегии <br> Интернет маркетинг",
+    "SEO-аудит <br> SEO на этапе создания сайта <br> Снятие санкций поисковых систем",
+    ""
+];
 
-//    //= include ./libraries/wow.js
 
-//= include ./components/menu.js
+function secondPlay() {
+    $("body").removeClass("play");
+    var aa = $("ul.secondPlay li.active");
 
-//= include ./components/open-form.js
+    if (aa.html() == undefined) {
+        aa = $("ul.secondPlay li").eq(0);
+        aa
+            .addClass("before")
+            .removeClass("active")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
 
-//= include ./components/scroll-menu.js
+    }
+    else if (aa.is(":last-child")) {
+        $("ul.secondPlay li").removeClass("before");
+        aa.addClass("before").removeClass("active");
+        aa = $("ul.secondPlay li").eq(0);
+        aa
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+    else {
+        $("ul.secondPlay li").removeClass("before");
+        aa
+            .addClass("before")
+            .removeClass("active")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+}
 
-//= include ./components/slider-carousel.js
+function minutePlay() {
+    $("body").removeClass("play");
+    var aa = $("ul.minutePlay li.active");
 
-//= include ./components/messages.js
+    if (aa.html() == undefined) {
+        aa = $("ul.minutePlay li").eq(0);
+        aa.addClass("before")
+            .removeClass("active")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
 
-//= include ./components/pop-ups.js
+    }
+    else if (aa.is(":last-child")) {
+        $("ul.minutePlay li").removeClass("before");
+        aa.addClass("before").removeClass("active");
+        aa = $("ul.minutePlay li").eq(0);
+        aa.addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+    else {
+        $("ul.minutePlay li").removeClass("before");
+        aa.addClass("before")
+            .removeClass("active")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+}
 
-//= include ./components/anchors.js
+(function recurse(counter) {
+    var catMsg = cat[counter],
+        subСatMsg = subСat[counter],
+        timer = setTimeout(function() {
+            recurse(counter + 1);
+            secondPlay();
+            minutePlay();
+        }, 4000);
 
-new WOW().init();
+    $('#category').html(catMsg).fadeIn(500).delay(3250).fadeOut(250);
+    $('#subcategory').html(subСatMsg).fadeIn(500).delay(3250).fadeOut(250);
+
+    delete cat[counter];
+    delete subСat[counter];
+
+    cat.push(catMsg);
+    subСat.push(subСatMsg);
+
+})(0);
