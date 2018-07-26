84a35b1f550a029c305264428e4b247ba9e7cef8
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-07-26 12:01:06 +0300

    change bread crumbs

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
 var mobile = '-mobile'
 // var mobile = ''
 
-var page = `services-mobile/complex${mobile}`
+var page = `services-mobile/web-production/web-production-second-level${mobile}`
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
