704f447e9c2892caccdbf17a15606903e36d33da
StadniukYevhen <stadniuk.yevhen@gmail.com>
2018-08-03 15:19:28 +0300

    create design mobile template

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
 var mobile = '-mobile';
 // var mobile = '';
 
-var page = `services-mobile/web-production${mobile}`
+var page = `services-mobile/design${mobile}`
 
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
